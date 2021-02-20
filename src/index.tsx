4725c0efb406c558298e8deefd3694b59f5ede08
Matthew Mullin <matthew.mullin@zenaptix.com>
2021-02-20 11:36:12 +0200

    Firebase Added

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import firebase from "firebase/app";
+
+// Use your config values here.
+firebase.initializeApp({
+  apiKey: "AIzaSyCMKBMCRfDkp4RrSDHC_JKbadsuX5s4o_M",
+  authDomain: "better-issues.firebaseapp.com",
+  projectId: "better-issues",
+  storageBucket: "better-issues.appspot.com",
+  messagingSenderId: "193364983508",
+  appId: "1:193364983508:web:84ffaed3f6e744edbe0541",
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
